65b72f00b7e91a809bea3480c54557ff6e89d0bd
Saksham Gera <sakshamgr3@gmail.com>
2024-04-30 21:12:51 -0500

    neo4j complete

---
+++ neo4j/README.md
@@ -0,0 +1,17 @@
+## Steps to reproduce
+
+- Download OSM data
+- Install Neo4j Desktop 5.12.0
+- Create DBMS in Neo4j Desktop and then create a new database
+- Install APOC plugin in Neo4j Desktop
+- Update the 'graphLoaderScript.ipynb' with the authentication information of the database and the location of the OSM data.
+- Run the Script.
+- Open Neo4j Browser and run the queries.
+
+
+### Benchmarking
+
+- Update the 'benchmarkingscript.py' with the authentication information of the database.
+- Install the dependecies: `pip install neo4j py2neo`
+- Run the script `python3 benchmarkingscript.py`
+- Modify the `num_runs` argument in `benchmark_query` function call to change the number of times you want to run the query.
--- neo4j/benchmarkingscript.py
+++ neo4j/benchmarkingscript.py
@@ -4,10 +4,10 @@ import time
 graph = Graph("bolt://localhost:7687", auth=("neo4j", "testadmin123"), name="neo5j")
 
 queries = {
-   # "Query 1": "WITH 0.0001 AS given_distance_squared, 37945950 AS given_node_id MATCH (origin:OSMNode {id: given_node_id}), (target:OSMNode) WHERE origin <> target AND 2 * 6371 * asin(sqrt(haversin(radians( origin.latitude - target.latitude )) + cos(radians( origin.latitude )) * cos(radians( target.latitude )) * haversin(radians( origin.longitude - target.longitude )))) <= given_distance_squared RETURN target.id AS TargetNodeID, target.latitude AS Latitude, target.longitude AS Longitude",
-    #"Query 2": "MATCH (start:OSMNode {id: 37945237}), (end:OSMNode {id: 37945238}), path = shortestPath((start)-[:CONNECTS_TO*]-(end)) WITH path, relationships(path) AS rels RETURN  reduce(totalCost = 0, r in rels | totalCost + r.path_length) AS totalPathLength",
-   # "Query 3": "MATCH (start:OSMNode {id: 37945950}) WITH start MATCH (end:OSMNode) WHERE end.tags CONTAINS 'cafe' WITH start, end MATCH path = shortestPath((start)-[:CONNECTS_TO*]-(end)) WITH path, relationships(path) AS rels RETURN path, reduce(totalCost = 0, r in rels | totalCost + r.path_length) AS totalPathLength ORDER BY totalPathLength LIMIT 1",
-#    "Query 4": "MATCH (start:OSMNode {id: 37945237}), (end:OSMNode {id: 37945238}), path = shortestPath((start)-[*]-(end)) RETURN path",
+    "Query 1": "WITH 0.0001 AS given_distance_squared, 37945950 AS given_node_id MATCH (origin:OSMNode {id: given_node_id}), (target:OSMNode) WHERE origin <> target AND 2 * 6371 * asin(sqrt(haversin(radians( origin.latitude - target.latitude )) + cos(radians( origin.latitude )) * cos(radians( target.latitude )) * haversin(radians( origin.longitude - target.longitude )))) <= given_distance_squared RETURN target.id AS TargetNodeID, target.latitude AS Latitude, target.longitude AS Longitude",
+    "Query 2": "MATCH (start:OSMNode {id: 37945237}), (end:OSMNode {id: 37945238}), path = shortestPath((start)-[:CONNECTS_TO*]-(end)) WITH path, relationships(path) AS rels RETURN  reduce(totalCost = 0, r in rels | totalCost + r.path_length) AS totalPathLength",
+    "Query 3": "MATCH (start:OSMNode {id: 37945950}) WITH start MATCH (end:OSMNode) WHERE end.tags CONTAINS 'cafe' WITH start, end MATCH path = shortestPath((start)-[:CONNECTS_TO*]-(end)) WITH path, relationships(path) AS rels RETURN path, reduce(totalCost = 0, r in rels | totalCost + r.path_length) AS totalPathLength ORDER BY totalPathLength LIMIT 1",
+    "Query 4": "MATCH (start:OSMNode {id: 37945237}), (end:OSMNode {id: 37945238}), path = shortestPath((start)-[*]-(end)) RETURN path",
     "Query 5": "MATCH (origin:OSMNode {id: 9661883082}),(target:OSMNode) WHERE target.tags CONTAINS 'cafe' AND 2 * 6371 * asin(sqrt(haversin(radians( origin.latitude - target.latitude )) + cos(radians( origin.latitude )) * cos(radians( target.latitude )) * haversin(radians( origin.longitude - target.longitude )))) < 1000 RETURN target"
 }
 
